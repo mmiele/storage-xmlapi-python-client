3d01219acd55f275d45efc2d983f17851c66c209
Michael Miele <mielem@gmail.com>
2014-03-11 17:07:43 -0700

    App main entry first release

---
+++ main.py
@@ -0,0 +1,87 @@
+'''
+    Command line example to interact with Google Cloud Storage (GCS) 
+    using XML API.
+    @note: The example displays the response for each request. 
+    The debug level is set to 1 by default. This is to display the 
+    request and response header for each request. This allows you to see the actual 
+    traffic on the wire as documented at: 
+    https://developers.google.com/storagedocs/reference-methods.
+'''
+__author__ = 'mielem@gmail.com'
+
+import logging
+import os
+import sys
+
+import gflags
+
+# Local imports.
+from gcs.simple_ui import GCS_SimpleUI 
+from gcs.commands import GCS_Command
+
+
+# Define the application's parameters the user must enter
+# at the activation time.
+FLAGS = gflags.FLAGS
+
+# The gflags module makes defining command-line options easy for
+# applications. Run this program with the '--help' argument to see
+# all the flags that it understands.
+LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+gflags.DEFINE_enum(
+    'logging_level', 'INFO', LOG_LEVELS, 'Set the level of logging detail.')
+
+
+def __init__app(debug_level):
+    '''
+      Initializes the application.
+
+    '''
+            
+    # Instantiate GCS_SimpleUI class.
+    ui = GCS_SimpleUI()
+    
+    # Instantiate GCS_Command class. 
+    gcs_commands = GCS_Command(debug_level)
+
+    # Display menu, get user's input and execute Google Cloud 
+    # Storage request based on user's selection.
+    ui.simple_ui(gcs_commands)
+
+def main(argv):
+    '''
+        Main entry point for the application.
+    '''
+  
+    try:
+        argv = FLAGS(argv)
+    except gflags.FlagsError, e:
+        print "%s\\nUsage: %s ARGS\\n%s" % (e, argv[0], FLAGS)
+        sys.exit(1)
+
+    # Set the logging according to the command-line flag
+    numeric_level = getattr(logging, FLAGS.logging_level.upper())
+    if not isinstance(numeric_level, int):
+        logging.error('Invalid log level: %s', FLAGS.logging_level)
+        logging.basicConfig(level=numeric_level)
+    
+    # Set the debug level based on the user's passed argument. 
+    if FLAGS.logging_level == 'DEBUG': 
+        # You can assign a debug level between 1 and 4.
+        # To assign this level to the authenticated (HTTP) client
+        # you must recreate the client (and related stored information)
+        # by reassigning the scope (selection s1).
+        debug_level = 1
+    else:
+        debug_level = 0
+
+    # print 'Debug level is: %s ' %debug_level
+  
+    # Initialize the application.
+    __init__app(debug_level)
+    
+   
+if __name__ == '__main__':
+  main(sys.argv)
+  
+ 
