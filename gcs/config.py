5f35fb86a399d4c6ab635f6c66f7f8ce4e26acff
Michael Miele <mielem@gmail.com>
2014-03-13 11:31:48 -0700

    First release.

---
+++ gcs/config.py
@@ -0,0 +1,27 @@
+'''
+    Contains data shared by all the modules.
+    @note: To use the data a module must use the 
+    statement: import config.
+
+'''
+__author__ = 'mielem@gmail.com'
+
+# Define allowed authentication scopes.
+scope_choices = {
+         'RW_SCOPE' : 'https://www.googleapis.com/auth/devstorage.read_write',
+         'WO_SCOPE' : 'https://www.googleapis.com/auth/devstorage.write_only',
+         'RO_SCOPE' : 'https://www.googleapis.com/auth/devstorage.read_only',
+         'FC_SCOPE' : 'https://www.googleapis.com/auth/devstorage.full_control'
+}
+
+
+# Global application data.
+app_data = {
+                'project_id' : None,
+                'scope' : None,
+                'http_client' : None,
+                'auth_http_client' : None
+}
+
+# Define the default HTTP verb.
+DEFAULT_METHOD = 'GET'
